02a552a0d910d34ab26321dce406ec25531a6e8f
Manpreet <manpreets95828@gmail.com>
2025-02-12 23:12:22 +0530

    Subset and String using Reccursion

---
+++ JAVA DSA/Recursion/String/src/Stream.java
@@ -0,0 +1,48 @@
+public class Stream {
+    public static void main(String[] args) {
+//        skip("" , "aabaccda");
+//        System.out.println(skip1("bccdah"));
+        System.out.println(skipappnotApple("bacapplecdah"));
+    }
+
+    private static void skip(String p, String up) {
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skip(p, up.substring(1));
+        }
+        else{
+            skip(p+ch, up.substring(1));
+        }
+    }
+
+    static String skip1(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            return skip1(up.substring(1)); // Skip 'a'
+        } else {
+            return ch + skip1(up.substring(1)); // Append valid characters
+        }
+    }
+
+    static String skipappnotApple(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipappnotApple(up.substring(3)); // Skip 'a'
+        } else {
+            return up.charAt(0) + skipappnotApple(up.substring(1)); // Append valid characters
+        }
+    }
+
+}
