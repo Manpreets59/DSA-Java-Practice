6bb190652f678e3d7eb815ee9384fe23ecb86705
Manpreet <manpreets95828@gmail.com>
2025-03-10 21:50:50 +0530

    Permutation files

--- JAVA DSA/Recursion/String/src/Permutation.java
+++ JAVA DSA/Recursion/String/src/Permutation.java
@@ -2,4 +2,20 @@
 // No of recursive calls at each level is given by size of processed +1
 
 public class Permutation {
+    public static void main(String[] args) {
+        permutations("", "abc");
+    }
+    static void permutations(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        for(int i = 0; i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            permutations(f+ch+s , up.substring(1));
+        }
+    }
 }
