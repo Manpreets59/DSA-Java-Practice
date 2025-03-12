7f7aab80966bb38ba89d44974ca77686c8bb353b
Manpreet <manpreets95828@gmail.com>
2025-03-12 16:41:05 +0530

    Return soulution of Maze using ArrayList

--- JAVA DSA/Recursion/backtracking/src/Maze.java
+++ JAVA DSA/Recursion/backtracking/src/Maze.java
@@ -1,9 +1,12 @@
 // Maze problem : It includes a number of way of reaching a specific index from a given index
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(count(3,3));
-        path("" , 3, 3);
+//        System.out.println(count(3,3));
+//        path("" , 3, 3);
+        System.out.println(pathRet("", 3,3));
     }
 
     static int count(int r, int c){
@@ -29,5 +32,24 @@ public class Maze {
         if(c>1){
             path(p+'R' , r , c-1 );
         }
+    }
+
+
+    // Returning the path in the ArrayList
+    static ArrayList<String> pathRet(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1){
+            list.addAll(pathRet(p + 'D' , r-1 , c));
+        }
+        if(c>1){
+            list.addAll(pathRet(p+'R' , r , c-1 ));
+        }
+        return list ;
     }
 }
