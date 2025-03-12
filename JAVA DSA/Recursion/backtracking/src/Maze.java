fe1ece2da9ed9020c2977523ee7e46925352e24f
Manpreet <manpreets95828@gmail.com>
2025-03-12 16:34:48 +0530

    Path that it follows

--- JAVA DSA/Recursion/backtracking/src/Maze.java
+++ JAVA DSA/Recursion/backtracking/src/Maze.java
@@ -2,7 +2,8 @@
 
 public class Maze {
     public static void main(String[] args) {
-
+        System.out.println(count(3,3));
+        path("" , 3, 3);
     }
 
     static int count(int r, int c){
@@ -13,5 +14,20 @@ public class Maze {
         int left = count(r-1,c);
         int right = count(r, c-1);
         return left+right;
+    }
+
+
+    // This is overview how it goes down and Right to reach target
+    static void path(String p, int r, int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p + 'D' , r-1 , c);
+        }
+        if(c>1){
+            path(p+'R' , r , c-1 );
+        }
     }
 }
