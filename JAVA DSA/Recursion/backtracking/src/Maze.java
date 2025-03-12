4787b0f3cd06b92c44c4c28ca67ae5bd88ab164b
Manpreet <manpreets95828@gmail.com>
2025-03-12 17:33:41 +0530

    Path with restriction

--- JAVA DSA/Recursion/backtracking/src/Maze.java
+++ JAVA DSA/Recursion/backtracking/src/Maze.java
@@ -7,7 +7,13 @@ public class Maze {
 //        System.out.println(count(3,3));
 //        path("" , 3, 3);
 //        System.out.println(pathRet("", 3,3));
-        System.out.println(pathRetDiagonal("" , 3 , 3));
+//        System.out.println(pathRetDiagonal("" , 3 , 3));
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+        pathRestriction("", board, 0,0);
     }
 
     static int count(int r, int c){
@@ -54,24 +60,42 @@ public class Maze {
         return list ;
     }
 
-//ading new variable for going diagonal to reach target
-static ArrayList<String> pathRetDiagonal(String p, int r, int c){
-    if(r==1 && c==1){
+//adding new condition for going diagonal to reach target
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
         ArrayList<String> list = new ArrayList<>();
-        list.add(p);
-        return list;
+        if(r>1 && c>1){
+            list.addAll(pathRetDiagonal(p + 'D' , r-1 , c-1));
+        }
+        if(r>1){
+            list.addAll(pathRetDiagonal(p + 'V' , r-1 , c));
+        }
+        if(c>1){
+            list.addAll(pathRetDiagonal(p+'H' , r , c-1 ));
+        }
+        return list ;
     }
 
-    ArrayList<String> list = new ArrayList<>();
-    if(r>1 && c>1){
-        list.addAll(pathRetDiagonal(p + 'D' , r-1 , c-1));
+    static void pathRestriction(String p,boolean[][] maze, int r, int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r<maze.length-1){
+            pathRestriction(p + 'D' ,maze, r+1 , c);
+        }
+        if(c<maze[0].length-1){
+            pathRestriction(p+'R' ,maze, r , c+1 );
+        }
     }
-    if(r>1){
-        list.addAll(pathRetDiagonal(p + 'V' , r-1 , c));
-    }
-    if(c>1){
-        list.addAll(pathRetDiagonal(p+'H' , r , c-1 ));
-    }
-    return list ;
-}
 }
