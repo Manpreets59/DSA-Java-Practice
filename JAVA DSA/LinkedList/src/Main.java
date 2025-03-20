cf41d6f90a8340b8aef4314a812053f08abd7108
Manpreet <manpreets95828@gmail.com>
2025-03-20 21:30:00 +0530

    Insert element at start of linkedlist

--- JAVA DSA/LinkedList/src/Main.java
+++ JAVA DSA/LinkedList/src/Main.java
@@ -4,5 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertFirst(31);
+
+        list.display();
+
     }
 }
