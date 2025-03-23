6743eab87a9f681c0a58db9b640fae4d6d153b5b
Manpreet <manpreets95828@gmail.com>
2025-03-23 20:59:11 +0530

    insert element in LinkedList using recursion

--- JAVA DSA/LinkedList/src/LL.java
+++ JAVA DSA/LinkedList/src/LL.java
@@ -60,6 +60,20 @@ public class LL {
         return val;
     }
 
+    // insertion using recursion
+    public void insertRec(int val , int index){
+        head = insertRec(val,index,head);
+    }
+    private Node insertRec(int val, int index, Node node){
+        if(index == 0){
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
+
     public int delete(int index){
         if(index == 0){
             return deleteFirst();
--- JAVA DSA/LinkedList/src/Main.java
+++ JAVA DSA/LinkedList/src/Main.java
@@ -5,24 +5,26 @@ public class Main {
     public static void main(String[] args) {
 //        Single Linked List
 
-//        LL list = new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(17);
-//        list.insertFirst(31);
-//
-//        list.insertLast(99);
-//
-//        list.insert(100, 3);
-//        list.display();
-//        System.out.println(list.deleteFirst() + " Is deleted ");
-//        list.display();
-//        System.out.println(list.deleteLast() + " Is deleted");
-//        list.display();
-//
-//        System.out.println(list.delete(3) + " is deleted element at index 3");
-//        list.display();
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertFirst(31);
+
+        list.insertLast(99);
+
+        list.insert(100, 3);
+        list.display();
+        System.out.println(list.deleteFirst() + " Is deleted ");
+        list.display();
+        System.out.println(list.deleteLast() + " Is deleted");
+        list.display();
+
+        System.out.println(list.delete(3) + " is deleted element at index 3");
+        list.display();
+        list.insertRec(88,3);
+        list.display();
 
 //        Double Linked List
 //        DLL list = new DLL();
@@ -37,13 +39,13 @@ public class Main {
 
 
 //        Circular LinkedList
-        CLL list = new CLL();
-        list.insert(23);
-        list.insert(45);
-        list.insert(5);
-        list.insert(12);
-        list.display();
-        list.delete(45);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(23);
+//        list.insert(45);
+//        list.insert(5);
+//        list.insert(12);
+//        list.display();
+//        list.delete(45);
+//        list.display();
     }
 }
