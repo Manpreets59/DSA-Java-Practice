e0f4f40217daeb13092e4cf82c02718be4d6b88f
Manpreet <manpreets95828@gmail.com>
2025-03-28 14:51:06 +0530

    LinkedList cycle part 2

--- JAVA DSA/LinkedList/src/LeetCode.java
+++ JAVA DSA/LinkedList/src/LeetCode.java
@@ -1,7 +1,5 @@
 public class LeetCode {
-    public static void main(String[] args) {
 
-    }
 
     // linked List cycle Questions
     // we use the idea that we have two pointer slow pointer and fast pointer, the slow pointer will move
@@ -23,4 +21,39 @@ public class LeetCode {
 //            return false;
 //        }
 //    }
+
+
+
+    //LinkedList Cycle Part2
+    // if length is zero no cycle exist
+    public ListNode detectCycle(ListNode head) {
+        int length = 0 ;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!= null && fast.next != null ){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow ){
+                lenght = lengthCycle(slow);
+                break;
+            }
+        }
+
+        // Find the start
+        ListNode f = head;
+        ListNode s = head;
+        while( length > 0){
+            s = s.next;
+            length--;
+        }
+        // keep moving both forward they meet at cycle start
+        while(f != s){
+            f= f.next;
+            s = s.next;
+
+        }
+        return s;
+
+
+    }
 }
