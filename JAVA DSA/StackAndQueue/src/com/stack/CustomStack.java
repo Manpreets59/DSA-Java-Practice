b441e3d5775ff74dd86d3afdcdbbbd0e0d82692b
Manpreet <manpreets95828@gmail.com>
2025-04-01 23:22:06 +0530

    Custom Stack

---
+++ JAVA DSA/StackAndQueue/src/com/stack/CustomStack.java
@@ -0,0 +1,18 @@
+package com.stack;
+
+public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int ptr = -1;
+
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+}
