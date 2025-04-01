2f8353288c863d57e37bb3970c97589e09c28243
Manpreet <manpreets95828@gmail.com>
2025-04-02 01:57:08 +0530

    Custom Circular Queue

--- JAVA DSA/StackAndQueue/src/com/Queue/QueueMain.java
+++ JAVA DSA/StackAndQueue/src/com/Queue/QueueMain.java
@@ -2,14 +2,25 @@ package com.Queue;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue =  new CustomQueue(5);
-        queue.insert(5);
+//        CustomQueue queue =  new CustomQueue(5);
+//        queue.insert(5);
+//        queue.insert(15);
+//        queue.insert(25);
+//        queue.insert(35);
+//        queue.insert(45);
+//        queue.display();
+//        System.out.println(queue.remove());
+//        queue.display();
+
+        CircularQueue queue = new CircularQueue(5);
         queue.insert(15);
         queue.insert(25);
         queue.insert(35);
         queue.insert(45);
+        queue.insert(55);
         queue.display();
         System.out.println(queue.remove());
+        queue.insert(99);
         queue.display();
     }
 }
