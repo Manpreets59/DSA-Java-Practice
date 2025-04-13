40dce790ae8772f8dcea68ba3f1731c7b96f9830
Manpreet <manpreets95828@gmail.com>
2025-04-13 12:02:00 +0530

    Binary Search Tree

--- JAVA DSA/Tree/src/BST.java
+++ JAVA DSA/Tree/src/BST.java
@@ -1,4 +1,4 @@
-public class BinarySearchTree {
+public class BST {
     public class Node{
         private int value;
         private int height;
@@ -13,10 +13,9 @@ public class BinarySearchTree {
             return value;
         }
     }
-
     private Node root;
 
-    public BinarySearchTree(){
+    public BST(){
 
     }
 
@@ -26,22 +25,27 @@ public class BinarySearchTree {
         }
         return node.height;
     }
-
     public boolean isEmpty(){
         return root == null;
     }
 
-    public void display(){
-        display(root, "Root Node: ");
+    public void insert (int value){
+
     }
-
-    private void display(Node node, String details){
+    private Node insert(int value, Node node){
         if(node == null){
-            return;
+            node = new Node(value);
+            return node;
         }
-        System.out.println(details + node.getValue());
-        display(node.left, "Left child of" + node.getValue() + " : ");
-        display(node.right, "Right child of" + node.getValue() + " : ");
 
+        if(value < node.value){
+            node.left = insert(value, node.left);
+        }
+
+        if(value > node.value){
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right))+1;
+        return node;
     }
 }
