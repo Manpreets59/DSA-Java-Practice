eb3ada73b4e02606835135af5a942e271255d127
Manpreet <manpreets95828@gmail.com>
2025-05-01 13:49:01 +0530

    Input Stream Reader

---
+++ JAVA DSA/StringBuffer/src/FileHandling.java
@@ -0,0 +1,27 @@
+// Streams -> Sequence of data it can be byte value and character value (Unicode)
+// java perform input and output through these streams. It is an abstraction that java provide
+// this stream is linked to physical device by java input output
+// java implement these within class hierarchy in java.io package
+// Types of streams in java
+// Byte Stream                 and        Character Stream
+// .
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileHandling {
+    public static void main(String[] args) {
+        try (InputStreamReader isr = new InputStreamReader(System.in)){
+            System.out.println("Enter some letters:");
+            int letters = isr.read();
+            while(isr.ready()){
+                System.out.println((char) letters);
+                letters = isr.read();
+            }
+            isr.close();
+            System.out.println();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
