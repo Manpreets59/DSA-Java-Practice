49d9e54097bfc976a973dc688d74cb2114a4a66a
Manpreet <manpreets95828@gmail.com>
2025-05-11 10:49:42 +0530

    Heap sort

---
+++ JAVA DSA/Heaps/src/Main.java
@@ -0,0 +1,16 @@
+// Heap data structure , priority Queue , Heapsort
+// 1) It is complete binary tree
+// 2) Every node has a value >= All of its children
+// Rules for Heap sort .
+// root = i = 1
+// parent(i) = i/2
+// left(i) = 2*i
+// right(i) = 2*i + 1
+// height = log(n)
+// Upheap method complexity  O(logn)
+// Heapdown method complexity O(logn).
+public class Main {
+    public static void main(String[] args) {
+        Heap<Integer> heap = new Heap<>();
+    }
+}
