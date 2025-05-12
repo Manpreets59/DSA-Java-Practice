883c3b39de3a75e05438c82cf6e37b441749e5f7
Manpreet <manpreets95828@gmail.com>
2025-05-12 15:49:40 +0530

    maxHeap complexity l

--- JAVA DSA/Heaps/src/Main.java
+++ JAVA DSA/Heaps/src/Main.java
@@ -8,7 +8,18 @@
 // right(i) = 2*i + 1
 // height = log(n)
 // Upheap method complexity  O(logn)
-// Heapdown method complexity O(logn).
+// Heapdown method complexity O(logn)
+// max heap property is Every node has a value >= All of its children
+// leaf node = n/2
+// one level above leafnode = n/4
+// one level above level 3 = n/8
+// root = 1 = log n
+// total time complexity n/4* c + n/8*2c + n/16*3c + .....+ 1c*logn
+// n/4 = 2^k
+// c2^k(1/2^0 + 1/2^1 + ... k+1/2^k ) bound by a constant
+// sum of i to infinity  = x/(1-x)^2 = o(n) .
+
+
 public class Main {
     public static void main(String[] args) throws Exception{
         Heap<Integer> heap = new Heap<>();
